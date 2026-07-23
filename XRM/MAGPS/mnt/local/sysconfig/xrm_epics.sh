7ad9a4dc0775320463dd9802a82c5d3a3f7ca881
dt100 <dt100@d-tacq.com>
2026-07-23 11:32:15 +0100

    PM changed to be on by default in QPMS and MAGPS XRM boxes

--- XRM/MAGPS/mnt/local/sysconfig/xrm_epics.sh
+++ XRM/MAGPS/mnt/local/sysconfig/xrm_epics.sh
@@ -35,7 +35,7 @@ export XRM_MODEL="XRM-MagPS"
 #export XRM_FMT_SIM=1
 export XRM_FMT_RX=1
 export XRM_SOE=1
-export XRM_PM=0
+export XRM_PM=1
 export XRM_INST1="STRATEGY=STR;REDIS_HOST=radish;REDIS_PORT=6379;"
 export IN1STR_cmd=/usr/local/bin/redis-acq400
 export XRM_INST2="STRATEGY=SPY;REDIS_HOST=radish;REDIS_PORT=6379;"
